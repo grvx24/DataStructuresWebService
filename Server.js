a238dc827a9bb800b2635d34ed842aa48358edf0
Szamaragdowy <springi775@gmail.com>
2018-07-05 08:18:45 +0200

    switch

--- Server.js
+++ Server.js
@@ -3,38 +3,72 @@ var fs = require('fs');
 
 
 var server = http.createServer(function (req, res) {
-    if (req.url == '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end();
-    }
-    else if (req.url == '/LinkedList.css') {
-        res.writeHead(200, {"Content-Type": "text/css"});
-        fs.readFile('LinkedList.css',null, function(error, data){
-            if(error){
-                res.writeHead(404);
-                res.write('File not found.');
-            }else{
-                res.write(data);
-            }
-            res.end();
-        });
-    }
-    else if (req.url == '/linkedlist') {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        fs.readFile('LinkedList.html',null, function(error, data){
-            if(error){
-                res.writeHead(404);
-                res.write('File not found.');
-            }else{
-                res.write(data);
-            }
-            res.end();
-        });
-    }
-    else
-        res.end('404 Invalid Request!');
 
+    var filename;
+    switch (req.url) {
+        case '/':
+
+            filename = "index.html";
+            break;
+        case '/index.css':
+
+            filename = "index.css";
+            break;
+
+        case '/linkedlist':
+
+            filename = "LinkedList.html";
+            break;
+
+        case '/LinkedList.css':
+            filename = "LinkedList.css";
+            break;
+
+        case '/graph':
+            filename = "Graph.html";
+            break;
+
+        case '/Graph.css':
+            filename = "Graph.css";
+            break;
+
+        case '/stack':
+            filename = "Stack_animation.html";
+            break;
+
+        case '/Stack_animation.css':
+            filename = "Stack_animation.css";
+            break;
+
+        case '/tree':
+            filename = "Tree.html";
+            break;
+
+        default:
+            res.end('404 Invalid Request!');
+            break;
+
+    }
+
+    if (filename) {
+       var isHtml =  filename.search("html");
+
+       if(isHtml>0){
+           res.writeHead(200, {"Content-Type": "text/html"});
+       }else{
+           res.writeHead(200, {"Content-Type": "text/css"});
+       }
+
+        fs.readFile(filename, null, function (error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write('File not found.');
+            } else {
+                res.write(data);
+            }
+            res.end();
+        });
+    }
 });
 
 server.listen(5000); //6 - listen for any incoming requests
