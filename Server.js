eeb27611901b2fd9e6ad321217516ac41368307d
Szamaragdowy <springi775@gmail.com>
2018-07-10 22:38:32 +0200

    refactor

--- Server.js
+++ Server.js
@@ -4,53 +4,120 @@ var fs = require('fs');
 var server = http.createServer(function (req, res) {
 
     var filename;
-    switch (req.url) {
-        case '/':
-            filename = "index.html";
-            break;
-        case '/index.css':
-            filename = "index.css";
-            break;
-        case '/linkedlist':
-            filename = "LinkedList.html";
-            break;
-        case '/LinkedList.css':
-            filename = "LinkedList.css";
-            break;
-        case '/graph':
-            filename = "Graph.html";
-            break;
-        case '/Graph.css':
-            filename = "Graph.css";
-            break;
-        case '/stack':
-            filename = "Stack_animation.html";
-            break;
-        case '/Stack_animation.css':
-            filename = "Stack_animation.css";
-            break;
-        case '/tree':
-            filename = "Tree.html";
-            break;
-        case '/canvas.png':
-            filename = "canvas.png";
-            break;
-        case '/LinkedList.js':
-            filename = "LinkedList.js";
-            break;
-        case '/graphAnimation':
-            filename = "Graph_Animation.html";
-            break;
-        case '/SaveToFile.js':
-            filename = "SaveToFile.js";
-            break;
-        case '/miserables.json':
-            filename = "miserables.json";
-            break;
-        default:
-            res.end('404 Invalid Request!');
-            break;
 
+    if(req.url.search("Styles")>0){
+
+        switch(req.url.replace('/Styles','')){
+            case '/MainStyles.css':
+                filename = "Styles/MainStyles.css";
+                break;
+            case '/Buttons.css':
+                filename = "Styles/Buttons.css";
+                break;
+        }
+    }else if (req.url.search("Images")>0){
+
+        switch(req.url.replace('/Images','')){
+            case '/canvas.png':
+                filename = "Images/canvas.png";
+                break;
+        }
+    }else{
+
+        switch (req.url) {
+            case '/':
+                filename = "index.html";
+                break;
+            case '/index.css':
+                filename = "index.css";
+                break;
+
+
+            case '/linkedlist':
+                filename = "Visualisation/LinkedList/LinkedList.html";
+                break;
+            case '/LinkedList.css':
+                filename = "Visualisation/LinkedList/LinkedList.css";
+                break;
+            case '/LinkedList.js':
+                filename = "Visualisation/LinkedList/LinkedList.js";
+                break;
+
+            case '/tree':
+                filename = "Visualisation/Tree/Tree.html";
+                break;
+            case '/Tree.css':
+                filename = "Visualisation/Tree/Tree.css";
+                break;
+            case '/Tree.js':
+                filename = "Visualisation/Tree/Tree.js";
+                break;
+
+
+            case '/graph':
+                filename = "Visualisation/Graph/Graph.html";
+                break;
+            case '/Graph.css':
+                filename = "Visualisation/Graph/Graph.css";
+                break;
+            case '/Graph.js':
+                filename = "Visualisation/Graph/Graph.js";
+                break;
+
+
+            case '/graphviz':
+                filename = "Visualisation/Graphviz/Graphviz.html";
+                break;
+            case '/Graphviz.css':
+                filename = "Visualisation/Graphviz/Graphviz.css";
+                break;
+            case '/Graphviz.js':
+                filename = "Visualisation/Graphviz/Graphviz.js";
+                break;
+
+
+            case '/hashtable':
+                filename = "Visualisation/HashTable/HashTable.html";
+                break;
+            case '/HashTable.css':
+                filename = "Visualisation/HashTable/HashTable.css";
+                break;
+            case '/HashTable.js':
+                filename = "Visualisation/HashTable/HashTable.js";
+                break;
+
+
+
+            case '/stackAnimation':
+                filename = "Animations/Stack/Stack_animation.html";
+                break;
+            case '/Stack_animation.css':
+                filename = "Animations/Stack/Stack_animation.css";
+                break;
+            case '/Stack_animation.js':
+                filename = "Animations/Stack/Stack_animation.js";
+                break;
+
+
+            case '/graphAnimation':
+                filename = "Animations/Graph/Graph_Animation.html";
+                break;
+            case '/Graph_Animation.css':
+                filename = "Animations/Graph/Graph_Animation.css";
+                break;
+            case '/Graph_Animation.js':
+                filename = "Animations/Graph/Graph_Animation.js";
+                break;
+
+
+            case '/SaveToFile.js':
+                filename = "SaveToFile.js";
+                break;
+
+            default:
+                res.end('404 Invalid Request!');
+                break;
+        }
     }
 
     if (filename) {
