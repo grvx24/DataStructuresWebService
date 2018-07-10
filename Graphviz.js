6dec2abd4270e0f68e158e2abc942457fa1f6603
Kamil Sagalara <grevax24@gmail.com>
2018-07-10 22:50:14 +0200

    Graphviz

--- Graphviz.js
+++ Graphviz.js
@@ -19,6 +19,35 @@ function RunExample()
         .renderDot(code);
 }
 
+function LinkedListExample() {
+
+    RemoveImage();
+    var code = 'digraph LinkedList{\n' +
+        '        rankdir=LR;\n' +
+        '        node [shape=record];\n' +
+        '        a [label="{ <data> 12 | <ref>  }", width=1.2]\n' +
+        '        b [label="{ <data> 99 | <ref>  }"];\n' +
+        '        c [label="{ <data> 37 | <ref>  }"];\n' +
+        '        d [label="{ <data> 99 | <ref>  }"];\n' +
+        '        e [label="{ <data> 37 | <ref>  }"];\n' +
+        '        f [label="{ <data> null }"];\n' +
+        '        a:ref:c -> b:data [arrowhead=vee, arrowtail=dot, dir=both, tailclip=false, arrowsize=1.2];\n' +
+        '        b:ref:c -> c:data [arrowhead=vee, arrowtail=dot, dir=both, tailclip=false];\n' +
+        '        c:ref:c -> d      [arrowhead=vee, arrowtail=dot, dir=both, tailclip=false];\n' +
+        '        d:ref:d -> e      [arrowhead=vee, arrowtail=dot, dir=both, tailclip=false];\n' +
+        '        e:ref:e -> f      [arrowhead=vee, arrowtail=dot, dir=both, tailclip=false];\n' +
+        '    }';
+
+    document.getElementById('Input').value=code;
+
+    d3.select("#graph").graphviz()
+        .fade(false)
+        .renderDot(code);
+
+
+}
+
+
 function RemoveImage() {
     var node = document.getElementById("graph");
     if(node.firstChild!=null)
@@ -42,8 +71,6 @@ function StartVisualization() {
     }catch (e) {
         alert(e.message);
     }
-
-
 }
 
 
